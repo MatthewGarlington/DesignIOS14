de54ad49467be15531d67b796d0e958af35cc6c2
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-17 15:55:16 -0500

    Initial Commit

---
+++ Shared/DesignIOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignIOS14App.swift
+//  Shared
+//
+//  Created by Matthew Garlington on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignIOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
